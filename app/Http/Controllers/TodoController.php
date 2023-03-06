9a420707b0b0f418c84490ba977fc844f16f54a7
Umayantha93 <umayantha93@gmail.com>
2023-03-06 14:13:26 +0530

    creating validations and displaying them on with nice message bar

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Todo;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -18,8 +19,26 @@ class TodoController extends Controller
     }
 
     public function store(Request $request) {
-        Todo::create($request->all());
 
+        $rules = [
+            'title' => 'required|max:255'
+        ];
+
+        $messages = [
+            'title.required' => "Title shouldn't be empty",
+            'title.max' => "Todo list should not be greater than 255 characters"
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return redirect()->back()
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+        // $request->validate([
+        //     'title' => 'Required'
+        // ]);
+
+        Todo::create($request->all());
         return redirect()->back()->with('message', 'Todo Created Successfully');
     }
 
