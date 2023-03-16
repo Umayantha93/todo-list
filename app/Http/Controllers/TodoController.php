f5eb52e8677fde1790e58a6a2f79cac0a36b5588
Umayantha93 <umayantha93@gmail.com>
2023-03-16 15:41:37 +0530

    completing todo list with adding delete function and some ui changes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -45,4 +45,16 @@ class TodoController extends Controller
 
         return redirect()->back()->with('message', 'Task marked as Completed!');
     }
+
+    public function incomplete(Todo $todo) {
+        $todo->update(['completed' => false]);
+
+        return redirect()->back()->with('message', "Task marked as Incomplete!");
+    } 
+
+    public function delete(Todo $todo) {
+        $todo->delete();
+
+        return redirect()->back()->with('message', "Delete Completed!");
+    }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,9 +1,11 @@
 @extends('todos.layout')
 
 @section('content')
-    <div class="flex justify-center border-b pb-4">
+    <div class="flex justify-between border-b pb-4 px-4">
         <h1 class="text-2xl">All The Todos</h1>
-        <a href="/todos/create" class="mx-5 py-1 px-1 bg-blue-400 cursor-pointer rounded text-white">Create New</a>
+        <a href="/todos/create" class="mx-5 py-1.5 text-blue-600 cursor-pointer">
+            <span class="fas fa-plus-circle" />
+        </a>
     </div>
    
     <ul class="my-5"> 
@@ -11,23 +13,32 @@
         @foreach($todos as $todo) 
         
             <li class="flex justify-between p-2">
+            <div>
+            @include('todos.complete-button')
+            </div>
             @if($todo->completed)
                 <p class="line-through">{{$todo->title}}</p>
             @else
                 <p>{{$todo->title}}</p>
             @endif
-                <div>
-                    
-                    <a href="{{'todos/'.$todo->id.'/edit'}}"  class="text-orange-400 cursor-pointer"><span class="fas fa-edit px-2" /> </a>
-                    @if($todo->completed)
-                        <span class="fas fa-check text-green-400 px-2">
-                    @else
-                        <span onclick="event.preventDefault(); document.getElementById('form-complete-{{$todo->id}}').submit()" class="fas fa-check text-gray-400 cursor-pointer px-2">
-                        <form style="display:none" id="{{'form-complete-'.$todo->id}}" method="post" action="{{ route('todo.complete', $todo->id) }}">
-                            @csrf
-                            @method('put')
-                        </form>
-                    @endif
+                <div>  
+                    <a href="{{'todos/'.$todo->id.'/edit'}}"  class="text-orange-400 cursor-pointer">
+                        <span class="fas fa-edit px-2" /> 
+                    </a>
+
+
+                    <span class="fas fa-trash text-red-500 px-2 cursor-pointer" 
+                    onclick="event.preventDefault();
+                        if(confirm('Are you really want to delete?')){
+                            document.getElementById('form-delete-{{$todo->id}}')
+                        .submit()
+                        } 
+                    "/> 
+
+                    <form style="display:none" id="{{'form-delete-'.$todo->id}}" method="post" action="{{ route('todo.delete', $todo->id) }}">
+                        @csrf
+                        @method('delete')
+                    </form>
                 </div>
             </li>
         
